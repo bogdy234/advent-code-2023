1e4ac3b9f9af30e03c65a58724b5089f8b2e7fba
Bogdan Filimon <bogdanfilimon@192-168-0-106.rdsnet.ro>
2023-12-05 22:20:06 +0200

    day 1, part 2

--- day1/day1.js
+++ day1/day1.js
@@ -1,16 +1,34 @@
 const fs = require("fs");
 
-const lines = fs.readFileSync("./input.txt", "utf-8").split("\n");
+const lines = fs.readFileSync("./input2.txt", "utf-8").split("\n");
+
+const numbersLetterMap = {
+  one: "one1one",
+  two: "two2two",
+  three: "three3three",
+  four: "four4four",
+  five: "five5five",
+  six: "six6six",
+  seven: "seven7seven",
+  eight: "eight8eight",
+  nine: "nine9nine",
+};
 
 let sum = 0;
 for (let line of lines) {
   const numbers = [];
+  let newLine = line;
 
-  for (let letter of line) {
+  Object.keys(numbersLetterMap).forEach((n) => {
+    newLine = newLine.replaceAll(n, numbersLetterMap[n]);
+  });
+
+  for (let letter of newLine) {
     if (!isNaN(letter)) {
       numbers.push(letter);
     }
   }
+
   sum += parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`);
 }
 
